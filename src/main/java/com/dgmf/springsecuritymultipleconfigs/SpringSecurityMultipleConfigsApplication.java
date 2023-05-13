61853915eec98233eedadc9e9c788a5f25a32b92
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-13 06:59:16 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/springsecuritymultipleconfigs/SpringSecurityMultipleConfigsApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.springsecuritymultipleconfigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMultipleConfigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMultipleConfigsApplication.class, args);
+	}
+
+}
