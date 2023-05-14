2bed7d9772316bc2711692ef353fd671d3d35185
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-14 06:38:58 +0200

    In the PostController, constructor injection of the PostRepository repository dependency. Implementation of the findAll() method returning a list of Posts

--- src/main/java/com/dgmf/springsecuritymultipleconfigs/controller/PostController.java
+++ src/main/java/com/dgmf/springsecuritymultipleconfigs/controller/PostController.java
@@ -1,9 +1,24 @@
 package com.dgmf.springsecuritymultipleconfigs.controller;
 
+import com.dgmf.springsecuritymultipleconfigs.model.Post;
+import com.dgmf.springsecuritymultipleconfigs.repository.PostRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+    private final PostRepository repository;
+
+    public PostController(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Post> findAll() {
+        return repository.findAll();
+    }
 }
