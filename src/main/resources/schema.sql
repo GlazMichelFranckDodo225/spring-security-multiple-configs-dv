8eb9a2cacce5f9c1c41d9fc94be915880be704f0
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-15 05:46:40 +0200

    Fixed DB connection problem.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-create table Post (
-    id int auto_increment primary key,
-    title varchar(255) not null,
-    content text not null,
+CREATE TABLE post (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
 );
 
-INSERT INTO Post(title, content) VALUES('Hello, world !', 'My First Blog Post');
+INSERT INTO post (title, content) VALUES ('Hello, world !', 'My First Blog Post');
