fcb1d2d590e04955ddaf1535411c6e75f24740f3
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-16 13:06:29 +0200

    Implementation of the "h2ConsoleSecurityFilterChain" which allows deactivating Spring Security on the url "/h2-console/**"

--- src/main/java/com/dgmf/springsecuritymultipleconfigs/config/SecurityConfig.java
+++ src/main/java/com/dgmf/springsecuritymultipleconfigs/config/SecurityConfig.java
@@ -1,9 +1,21 @@
 package com.dgmf.springsecuritymultipleconfigs.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll())
+                .csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
+                .headers(headers -> headers.frameOptions().disable())
+                .build();
+    }
 }
