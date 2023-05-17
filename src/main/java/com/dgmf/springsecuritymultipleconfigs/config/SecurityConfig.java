e7389f95131707dea9f1db8f218d3775b1ab1a0c
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-17 06:12:04 +0200

    Implementation of the filter "apiSecurityFilterChain(HttpSecurity http)" on the Endpoint "/api/**". Added the "@Order(1)" annotation on the "h2ConsoleSecurityFilterChain(HttpSecurity http)" filter to ensure its primacy over other filters

--- src/main/java/com/dgmf/springsecuritymultipleconfigs/config/SecurityConfig.java
+++ src/main/java/com/dgmf/springsecuritymultipleconfigs/config/SecurityConfig.java
@@ -2,15 +2,35 @@ package com.dgmf.springsecuritymultipleconfigs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    // "/api/**" ==> Basic Authentication
     @Bean
+    SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // Securization of the Endpoint "/api/**"
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").authenticated())
+                // Disable Session Management
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Basic protection of the Endpoint
+                .httpBasic(withDefaults())
+                .build();
+    }
+
+    //  H2 console ==> Spring Security disabled
+    @Bean
+    @Order(1) // Spring Security will run this filter first
     SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll())
